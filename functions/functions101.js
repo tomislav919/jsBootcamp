cc319a098c54624d1c447dd99b6c31d03dfb17da
Tomislav Lukanic <tomislav919@gmail.com>
2020-01-16 15:52:01 +0100

    Added functions lesson

--- functions/functions101.js
+++ functions/functions101.js
@@ -1,1 +1,29 @@
-//comment
+//Function - input (argument), code, output (return value)
+
+let greetUser = function()
+{
+    console.log('Welcome user')
+}
+
+greetUser()
+
+
+let square = function(num)
+{
+    let result = num * num
+    console.log(result)
+}
+
+square(3)
+square(10)
+
+//challange
+
+let converter = function (cel)
+{
+    let result = (cel * ( 9 / 5)) + 32
+    return result
+}
+
+let temp = converter(10)
+console.log(temp)
