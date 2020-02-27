b038ae1eb80e8f05ab8a9959ca5ea93cd34c907b
Tomislav Lukanic <tomislav919@gmail.com>
2020-02-27 09:24:02 +0100

    Array basics

--- arrays/notes.js
+++ arrays/notes.js
@@ -7,3 +7,27 @@ console.log(notes[15]); // Undefined
 console.log(notes[notes.length - 1]); // Last item in array
 console.log(notes[notes.length - 2]); // Second to last item in array
 
+notes.push('My new note');
+console.log(notes);
+
+notes.pop(); //removes last note
+console.log(notes);
+
+let lastItem = notes.pop(); //removes last items and returns that item
+console.log(lastItem);
+console.log(notes);
+
+
+console.log(notes.shift()); // removes the first item in array and returns its value
+console.log(notes.unshift('lala land')); // add the first item in array
+console.log(notes);
+
+
+const notes2 = ['Note 1', 'Note 2', 'Note 3'];
+
+notes2.splice(1,1); // starts at item 1 and deletes 1 item
+notes2.splice(1,0, 'This is the new item'); // starts at item 1 and adds 1 item after it
+console.log(notes2);
+
+notes2[2] = 'note 3 is renamed';
+console.log(notes2);
--- arrays/todo.js
+++ arrays/todo.js
@@ -9,5 +9,17 @@ console.log(`Todo: ${todos[0]}!`);
 console.log(`Todo: ${todos[todos.length - 1]}!`);
 console.log(`Todo: ${todos[todos.length - 2]}!`);
 
+const todos2 = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Excercise'];
+
+//Delete the 3rd item
+//Add a new item to the end
+// Remove the first item from the list
+
+console.log(todos2);
+todos2.splice(2,1); //delete 3rd
+todos2.shift(); //removes the first item
+todos2.push('Jump right in'); //adds new item
+
+console.log(todos2);
 
 
