e47659e6223a3c23902e97fd9a32fb2490877597
Tomislav Lukanic <tomislav919@gmail.com>
2020-03-01 09:36:29 +0100

    More FOR loop excercises

--- arrays/loops.js
+++ arrays/loops.js
@@ -16,6 +16,16 @@ todos.forEach(function(item, index){
 });
 
 // FOR loop
-for (let count = 0; count <= 2; count++){
+for (let count = 0; count <= 2; count++){ //Counting 0 1 2
     console.log(count);
+}
+
+for (let count = notes.length - 1; count >= 0; count--){ //Counting 2 1 0
+    console.log(count);
+}
+
+const todos2 = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Excercise'];
+
+for (let count = 0; count < todos2.length; count++){
+    console.log(`${count + 1}. ${todos2[count]}`);
 }
